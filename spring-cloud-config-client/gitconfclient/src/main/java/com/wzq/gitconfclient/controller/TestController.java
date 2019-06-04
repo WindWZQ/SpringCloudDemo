8764d3b0026c06322557e492d75598c6bdae266e
wangzhenqi <wangzhenqi>
2019-06-04 15:05:07 +0800

    modify config

--- spring-cloud-config-client/gitconfclient/src/main/java/com/wzq/gitconfclient/controller/TestController.java
+++ spring-cloud-config-client/gitconfclient/src/main/java/com/wzq/gitconfclient/controller/TestController.java
@@ -1,4 +1,24 @@
 package com.wzq.gitconfclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @Value("${version}")
+    private String version;
+
+    @Value("${content}")
+    private String content;
+
+    @RequestMapping("hello")
+    public String from() {
+        return profile + " " + version + " " + content;
+    }
+
 }

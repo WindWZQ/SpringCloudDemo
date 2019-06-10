00e44dcbfd51d012b2df080884c71a3fdd627f53
wangzhenqi <wangzhenqi>
2019-06-10 14:53:27 +0800

    init gateway

--- spring-cloud-eureka-client/user/src/main/java/com/wzq/user/controller/LoginController.java
+++ spring-cloud-eureka-client/user/src/main/java/com/wzq/user/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("sign")
 public class LoginController {
 
     @GetMapping("login")
